c999cf4c95cbe722750000526136e84e9e704de7
Kevin <kevinworster@live.de>
2023-02-24 14:31:51 +0100

    init gradle spring boot

---
+++ DigAs/src/main/java/com/github/primitiveengineering/digas/DigAsApplication.java
@@ -0,0 +1,13 @@
+package com.github.primitiveengineering.digas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigAsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigAsApplication.class, args);
+    }
+
+}
---
+++ DigAs/src/test/java/com/github/primitiveengineering/digas/DigAsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.primitiveengineering.digas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigAsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
