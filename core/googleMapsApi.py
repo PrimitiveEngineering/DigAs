cb91fe7105070678d63e0997e6a3f3a1dce03ab4
Georg <georg.junemann@web.de>
2023-03-08 20:33:21 +0100

    Added arrival_time for google maps api

--- core/googleMapsApi.py
+++ core/googleMapsApi.py
@@ -3,6 +3,7 @@ import json
 import os
 from dotenv import load_dotenv
 import time
+from datetime import timezone
 
 load_dotenv()
 
@@ -11,18 +12,26 @@ class GoogleMapsApiException(Exception):
     Is raised when a Parameter in the Google Maps API is invalid
     """
 
+#
+
 class GoogleMapsApi:
     """
     Google Maps API
 
     Arguments:
-    - origin (required)
-    - destination (required)
-    - travelMode (optional: driving, driving, transit, walking)
+    - origin            (required)
+    - destination       (required)
+    - travelMode        (optional; types: driving, driving, transit, walking)
+    - arrivalTime       (optional; only available when travelMode is transit;
+                        type: datetime(year, month, day, hour, minutes) (all as integer))
+    - returnStringOnly  (default: True; if False: return Values for distance and duration
+                        as integer in meter and seconds)
 
     Returns:
     - distance as tts string
+    - distance as integer in meter (if returnStringOnly=False)
     - destination as tts string
+    - destination as integer in seconds (if returnStringOnly=False)
     """
 
     baseUrl = 'https://maps.googleapis.com/maps/api/distancematrix/json?key=' + os.getenv('GOOGLE_API_KEY')
@@ -44,19 +53,30 @@ class GoogleMapsApi:
     payload = {}
     headers = {}
 
-    def googleMapsApiRequest(self, origin, destination, travelMode='driving'):
+    def googleMapsApiRequest(self, origin, destination, travelMode='driving', arrivalTime=None, returnStringsOnly='True'):
 
-        url = self.parameterValid(travelMode, origin, destination, self.baseUrl)
+        url = self.parameterValid(travelMode, origin, destination, arrivalTime, self.baseUrl)
 
         response = requests.request("GET", url, headers=self.headers, data=self.payload)
         responseJson = json.loads(response.text)
 
-        return self.getDistance(responseJson), self.getDuration(responseJson)
+        print(response.text)
 
-    def parameterValid(self, travelMode, origin, destination, url):
+        if returnStringsOnly:
+            return self.getDistance(responseJson), self.getDuration(responseJson)
+        else:
+            return self.getDistance(responseJson), self.getDistanceValue(responseJson), self.getDuration(responseJson), self.getDurationValue(responseJson)
+
+
+    def parameterValid(self, travelMode, origin, destination, arrivalTime, url):
 
         self.travelModeValid(travelMode)
         url += '&mode=' + travelMode
+
+        if travelMode == 'transit' and not arrivalTime is None:
+            arrivalTime = int(arrivalTime.replace(tzinfo=timezone.utc).timestamp())
+            self.arrivalTimeValid(arrivalTime)
+            url += '&arrival_time=' + str(arrivalTime)
         
         self.originIsString(origin)
         url += '&origins=' + origin
@@ -66,21 +86,31 @@ class GoogleMapsApi:
 
         return url
     
+
     def travelModeValid(self, travelMode):
         if travelMode:
             if travelMode not in self.available_travel_modes:
                 raise GoogleMapsApiException(f'travelMode is from no available type (Value: {travelMode})')
         return True
     
+    
+    def arrivalTimeValid(self, arrivalTime):
+        if arrivalTime <= int(time.time()):
+            raise GoogleMapsApiException(f'ArrivalTime is prior to the current date')
+        return True
+    
+
     def originIsString(self, origin):
         if not isinstance(origin, str):
             raise GoogleMapsApiException(f'Origin is not a string')
         return True
     
+
     def destinationIsString(self, destination):
         if not isinstance(destination, str):
             raise GoogleMapsApiException(f'Destination is not a string')
         return True
+
 
     def getDuration(self, responseJson):
         duration = responseJson['rows'][0]['elements'][0]['duration']['text']
@@ -91,6 +121,7 @@ class GoogleMapsApi:
         
         return duration
 
+
     def getDistance(self, responseJson):
         distance = responseJson['rows'][0]['elements'][0]['distance']['text']
 
@@ -100,3 +131,15 @@ class GoogleMapsApi:
             distance = distance.replace('m', 'meter')
 
         return distance
+    
+
+    def getDurationValue(self, responseJson):
+        durationValue = int(responseJson['rows'][0]['elements'][0]['duration']['value'])
+        
+        return durationValue
+
+
+    def getDistanceValue(self, responseJson):
+        distanceValue = int(responseJson['rows'][0]['elements'][0]['distance']['value'])
+
+        return distanceValue
