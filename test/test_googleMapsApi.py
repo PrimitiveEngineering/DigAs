8fb0c727c2547bb03def0a0136a47a42c668b800
Georg <georg.junemann@web.de>
2023-03-08 20:33:48 +0100

    Added example for google maps api call and response

--- test/test_googleMapsApi.py
+++ test/test_googleMapsApi.py
@@ -27,3 +27,38 @@ class TestGoogleMapsApi(TestCase):
 
     def test_selfTest(self):
         self.assertEqual(True,True) #We love it!!!
+
+
+'''
+Anfrage:
+print(gMaps.googleMapsApiRequest("Filderstadt", "Stuttgart", 'transit', arrivalTime=datetime(2023, 3, 12, 3)))
+
+{
+    "destination_addresses" : [ "Stuttgart, Germany" ],
+    "origin_addresses" : [ "Filderstadt, Germany" ],
+    "rows" : [
+        {
+            "elements" : [
+            {
+                "distance" : {
+                    "text" : "22.9 km",
+                    "value" : 22851
+                },
+                "duration" : {
+                    "text" : "1 hour 9 mins",
+                    "value" : 4113
+                },
+                "status" : "OK"
+            }
+            ]
+        }
+    ],
+    "status" : "OK"
+}
+
+Output:
+('22.9 kilometer', '1 stunde 9 minuten')
+
+
+Für weitere Konfigs -> Class description
+'''
