5b502176ab074563642e3553da2ccbc40214084e
Georg Junemann <georg.junemann@mercedes-benz.com>
2023-03-09 09:23:52 +0100

    Added jokeApi.py

---
+++ joke/jokeApi.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+
+class JokeApi:
+
+    payload = {}
+    headers = {}
+
+    def jokeApiRequest(self):
+
+        url = 'https://v2.jokeapi.dev/joke/Any?type=single'
+
+        response = requests.request("GET", url, headers=self.headers, data=self.payload)
+        responseJson = json.loads(response.text)
+
+        return self.getJoke(responseJson)
+    
+    
+    def getJoke(self, json):
+
+        return json['joke']
