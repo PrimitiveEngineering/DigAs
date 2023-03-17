b881bcd5a492539ef9a32c466132bc3e0141cdcc
maxsy <maxsydneyduerr@icloud.com>
2023-03-17 11:49:58 +0100

    Refactored to fit singleton and strategy pattern

--- core/speechToText.py
+++ core/speechToText.py
@@ -1,8 +1,32 @@
+from abc import ABC, abstractmethod
+import logging
 import speech_recognition as sr
 
 
-class SpeechToTextInterface:
-    def get_speech_test(self):
+class Speech2TextInterface(ABC):
+    __instance = None
+
+    # singleton pattern
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
+
+    @abstractmethod
+    def trigger(self):
+        pass
+
+
+class _OfflineS2T(Speech2TextInterface):
+    """
+    Offline Speech to Text functionality. Worse audio recognition but it's always available.
+    """
+
+    def trigger(self):
+        """
+        Outputs the entered audio as text.
+        :return: understood text
+        """
 
         # obtain audio from the microphone
         r = sr.Recognizer()
@@ -19,16 +43,16 @@ class SpeechToTextInterface:
         except sr.RequestError as e:
             print("Sphinx error; {0}".format(e))
 
-        # recognize speech using Google Speech Recognition
-        print("Google:")
-        try:
-            print("\"" + r.recognize_google(audio) + "\"")
-        except sr.UnknownValueError:
-            print("Google Speech Recognition could not understand audio")
-        except sr.RequestError as e:
-            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+class _GoogleS2T(Speech2TextInterface):
+    """
+    Offline Text to Speech functionality. Better audio recognition but it needs an internet connection.
+    """
 
-    def get_speech(self):
+    def trigger(self):
+        """
+        Outputs the entered audio as text.
+        :return: understood text
+        """
 
         # obtain audio from the microphone
         r = sr.Recognizer()
@@ -46,6 +70,46 @@ class SpeechToTextInterface:
         except sr.RequestError as e:
             return ("Could not request results from Google Speech Recognition service; {0}".format(e))
 
-s2t = SpeechToTextInterface()
 
-print("Output: " + s2t.get_speech())
+
+class Speech2TextFactory(object):
+    """
+    The Factory class for creating the special S2T interfaces.
+    """
+
+    def __new__(cls, mode):
+        av_mode = {
+            "offline": _OfflineS2T(),
+            "google": _GoogleS2T(),
+        }
+        mode = str.lower(mode)
+        if mode in av_mode.keys():
+            return av_mode.get(mode)
+        else:
+            logging.error(f"Speech to text mode {mode} not found. Falling back to OfflineS2T.")
+            return av_mode.get("offline")
+
+
+class Speech2TextService:
+    """
+    This class should be used mainly for the speech to text functionality.
+    It is the combination of a singleton and strategy pattern
+    """
+
+    __instance = None
+    __s2t_interface = None
+
+    # singleton pattern
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
+
+    def __init__(self, mode):
+        self.change_mode(mode)
+
+    def change_mode(self, mode):
+        self.__s2t_interface = Speech2TextFactory(mode)
+
+    def trigger(self, text, ssml):
+        self.__s2t_interface.trigger()
