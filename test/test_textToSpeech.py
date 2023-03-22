a934ca1874e657c228258bdf63d1905269e0d031
Tomasz Kania <101673515+TomaszKania2000@users.noreply.github.com>
2023-03-22 12:19:51 +0100

    Correcting Tests to Design Pattern Adding new Tests Finishing touches

--- test/test_textToSpeech.py
+++ test/test_textToSpeech.py
@@ -6,19 +6,34 @@
     license:    MIT
 """
 from unittest import TestCase
-from core.textToSpeech import AzureT2S
+from core.textToSpeech import _AzureT2S, _OfflineT2S, Text2SpeechFactory
 
-AT2S = AzureT2S()
+AT2S = _AzureT2S()
+OT2S = _OfflineT2S()
+T2SF = Text2SpeechFactory
 
 class TestTextToSpeech(TestCase):
-    def test_build_ssml_headtail(self):
+
+    def test_OT2S_remove_ssml(self):
+        CorrectString = "Nobody expects the Spanish Inquisition"
+        StringToTest = "Nobody expects the <Indish>Spanish Inquisition"
+        self.assertEqual(CorrectString,OT2S.remove_ssml(StringToTest))
+
+    def test_AT2S_build_ssml_headtail(self):
+        CorrectString = f"<speak xmlns=\"http://www.w3.org/2001/10/synthesis\" xmlns:mstts=\"http://www.w3.org/2001/mstts\" xmlns:emo=\"http://www.w3.org/2009/10/emotionml\" version=\"1.0\" xml:lang=\"en-US\"><voice name=\"VOICE\"><s /><prosody rate=\"PROSODY\">TEXT</prosody><s /></voice></speak>"
         voice = "VOICE"
         prosody = "PROSODY"
         text = "TEXT"
-        CorrectString = f"<speak xmlns=\"http://www.w3.org/2001/10/synthesis\" xmlns:mstts=\"http://www.w3.org/2001/mstts\" xmlns:emo=\"http://www.w3.org/2009/10/emotionml\" version=\"1.0\" xml:lang=\"en-US\"><voice name=\"VOICE\"><s /><prosody rate=\"PROSODY\">TEXT</prosody><s /></voice></speak>"
-        self.assertEqual(CorrectString,AT2S.build_ssml_headtail(text,voice,prosody))
+        self.assertEqual(CorrectString, AT2S.build_ssml_headtail(text,voice,prosody))
 
-    def test_remove_ssml(self):
-        CorrectString = "Nobody expects the Spanish Inquisition"
-        StringToTest = "Nobody expects the <Indish>Spanish Inquisition"
-        self.assertEqual(CorrectString,AT2S.remove_ssml(StringToTest))
+    def test_T2SF_new_azure(self):
+        self.assertEqual(AT2S, T2SF.__new__(T2SF,"azure"))
+
+    def test_T2SF_new_offline(self):
+        self.assertEqual(OT2S, T2SF.__new__(T2SF,"offline"))
+
+    def test_T2SF_new_fail(self):
+        self.assertEqual(OT2S, T2SF.__new__(T2SF,"BiggusDickus"))
+
+
+
