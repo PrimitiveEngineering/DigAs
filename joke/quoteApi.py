c0864eb33149173158f88b7fe001a39b49312969
Georg Junemann <georg.junemann@mercedes-benz.com>
2023-03-26 18:47:47 +0200

    Changed to python syntax

--- joke/quoteApi.py
+++ joke/quoteApi.py
@@ -3,35 +3,33 @@ import json
 
 
 class QuoteApi:
-    '''
-    Quote API
-
-    Arguments:
-    None
-
-    Returns:
-    - quote as string
-    - author name as string
-    '''
 
     payload = {}
     headers = {}
 
-    def quoteApiRequest(self):
+    def quote_api_request(self):
+        """
+            Quote API
+
+            Arguments:
+            None
+
+            Returns:
+            - quote as string
+            - author name as string
+        """
 
         url = 'https://api.quotable.io/random'
 
         response = requests.request("GET", url, headers=self.headers, data=self.payload)
-        responseJson = json.loads(response.text)
+        response_json = json.loads(response.text)
         
-        return self.getContent(responseJson), self.getAuthor(responseJson)
+        return self.get_content(response_json), self.get_author(response_json)
     
-    
-    def getContent(self, json):
+    def get_content(self, json):
 
         return json['content']
-    
 
-    def getAuthor(self, json):
+    def get_author(self, json):
 
         return json['author']
