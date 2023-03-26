21b7b2a02057419f389d23d500fec6cb568195e7
Georg Junemann <georg.junemann@mercedes-benz.com>
2023-03-26 17:56:54 +0200

    Added blacklist option and fixed formatting error

--- joke/jokeApi.py
+++ joke/jokeApi.py
@@ -1,31 +1,68 @@
 import requests
 import json
 
+class JokeApiBlacklistOptionNotFound(Exception):
+    """
+    Is raised when a blacklist option is not found
+    """
 
 class JokeApi:
-    '''
-    Joke API
 
-    Arguments:
-    None
-
-    Returns:
-    - single line joke as string
-    '''
+    blacklist_options = [
+        'nsfw',
+        'religious',
+        'political',
+        'racist',
+        'sexist',
+        'explicit'
+    ]
 
     payload = {}
     headers = {}
 
-    def jokeApiRequest(self):
+    def jokeApiRequest(self, blacklist=None):
+        """
+            Joke API
+
+            Arguments:
+            - list of blacklist flags   (optional; flags: nsfw, religious, political, racist, sexist, explicit)
+
+            Returns:
+            - single line joke as string
+        """
 
         url = 'https://v2.jokeapi.dev/joke/Any?type=single'
 
+        blacklistString = self.getBlacklistString(blacklist)
+
+        url += blacklistString
+
         response = requests.request("GET", url, headers=self.headers, data=self.payload)
+
         responseJson = json.loads(response.text)
 
         return self.getJoke(responseJson)
     
-    
+
+    def getBlacklistString(self, blacklist):
+
+        if blacklist is None:
+            return ''
+
+        blacklistString = '&blacklistFlags='
+
+        for bl in blacklist:
+            if bl not in self.blacklist_options:
+                raise JokeApiBlacklistOptionNotFound(f'Blacklist option {bl} not an available option')
+
+        blacklistString += ','.join(blacklist)
+
+        return blacklistString
+
+
     def getJoke(self, json):
 
-        return json['joke']
+        jokeRaw = json['joke']
+        joke = jokeRaw.replace('\n', '')
+
+        return joke
