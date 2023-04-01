a04685f4adf655c509308ea7636f1be06f27b0db
Georg <georg.junemann@web.de>
2023-04-01 18:28:58 +0200

    Added food api request with set ingredients

--- cooking/foodApi.py
+++ cooking/foodApi.py
@@ -1,7 +1,7 @@
 import os
 import requests
 import json
-
+import random
 from dotenv import load_dotenv, find_dotenv
 
 
@@ -18,6 +18,29 @@ class FoodApi:
         load_dotenv(find_dotenv())
         self.api_key = os.getenv('SPOONACULAR_API_KEY')
 
+    def food_api_find_by_ingredients_request(self, ingredient_list, ranking=2):
+        """
+        Arguments:
+        - list containing all ingredients
+        - Whether to maximize used ingredients (1) or minimize missing ingredients (2) first. (Default: 2)
+
+        Returns:
+        - name of the recipe as string
+        - dictionary containing the name of a used ingredient with the needed measures
+        - array of steps for the recipe (it is possible that no instructions are available)
+        """
+
+        ingredients_as_csv = self.convert_ingredient_list_to_csv(ingredient_list)
+
+        url = 'https://api.spoonacular.com/recipes/findByIngredients?number=25&limitLicense=true&ranking=' + \
+              str(ranking) + '&ignorePantry=true&apiKey=' + self.api_key + '&ingredients=' + ingredients_as_csv
+
+        response = requests.request("GET", url, headers=self.headers, data=self.payload)
+        response_json = json.loads(response.text)
+        recipe_id = response_json[random.randint(0, len(response_json)-1)]['id']
+
+        return self.extract_recipe_informations(recipe_id)
+
     def food_api_random_request(self):
         """
         Arguments:
@@ -26,16 +49,27 @@ class FoodApi:
         Returns:
         - name of the recipe as string
         - dictionary containing the name of a used ingredient with the needed measures
-        - array of steps for the recipe
+        - array of steps for the recipe (it is possible that no instructions are available)
         """
 
-        url = 'https://api.spoonacular.com/recipes/random?number=1&limitLicense=true&apiKey=' + self.api_key
+        url = 'https://api.spoonacular.com/recipes/random?number=25&limitLicense=true&apiKey=' + self.api_key
 
         response = requests.request("GET", url, headers=self.headers, data=self.payload)
         response_json = json.loads(response.text)
+        recipe_id = response_json['recipes'][random.randint(0, len(response_json['recipes'])-1)]['id']
 
-        recipe = response_json['recipes'][0]
-        analyzed_instructions = recipe['analyzedInstructions'][0]['steps']
+        return self.extract_recipe_informations(recipe_id)
+
+    def extract_recipe_informations(self, recipe_id):
+
+        url = 'https://api.spoonacular.com/recipes/' + str(recipe_id) + '/information?apiKey=' + self.api_key
+
+        response = requests.request("GET", url, headers=self.headers, data=self.payload)
+        recipe = json.loads(response.text)
+
+        analyzed_instructions = []
+        if recipe['analyzedInstructions']:
+            analyzed_instructions = recipe['analyzedInstructions'][0]['steps']
         extended_ingredients = recipe['extendedIngredients']
 
         recipe_name = recipe['title']
@@ -43,6 +77,9 @@ class FoodApi:
         step_descriptions = self.extract_steps_to_array(analyzed_instructions)
 
         return recipe_name, ingredient_dic, step_descriptions
+
+    def convert_ingredient_list_to_csv(self, ingredient_list):
+        return ",".join(ingredient_list)
 
     def extract_steps_to_array(self, analyzed_instructions):
         step_descriptions = []
@@ -75,3 +112,10 @@ class FoodApi:
         if float_value.is_integer():
             return int(float_value)
         return float_value
+
+
+f = FoodApi()
+
+food_list = ['apple', 'sugar']
+
+print(f.food_api_random_request())
