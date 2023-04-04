87e33c58996d2c4997cf4e256c8710d700ba1ea8
Georg <georg.junemann@web.de>
2023-04-04 20:03:52 +0200

    Fixed location globally

--- joke/fortuneCookieService.py
+++ joke/fortuneCookieService.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 
 class FortuneCookieService:
@@ -12,18 +13,18 @@ class FortuneCookieService:
     - quote as string
     """
 
-
     fortunes = []
 
     def __init__(self) -> None:
-        f = open("joke/fortuneList.txt", "r", encoding='utf8')
+        f = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "fortuneList.txt"), "r", encoding='utf8')
         lines = f.readlines()
 
         for line in lines:
             self.fortunes.append(line)
 
-    def fortune_cookie_service_request(self):
+    def fortune_cookie_service_request(self, joke_number=None):
 
-        rand = random.randint(0, len(self.fortunes))
+        if joke_number is None:
+            joke_number = random.randint(0, len(self.fortunes))
 
-        return self.fortunes[rand]
+        return self.fortunes[joke_number]
