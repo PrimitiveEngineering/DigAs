2799777999fade90c6a6714d6f5f3101bb48fd46
Tomasz Kania <101673515+TomaszKania2000@users.noreply.github.com>
2023-04-04 19:11:53 +0200

    adding test_fortuneCookieService.py adding fortuneList.txt in test folder as workaround

---
+++ test/test_fortuneCookieService.py
@@ -0,0 +1,21 @@
+""" test_fortuneCookieService
+    * all basic-tests for fortuneCookie-Service are implemented here
+    author:     inf20086@lehre.dhbw-stuttgart.de
+    date:       04.04.2023
+    version:    0.0.1
+    license:    MIT
+"""
+
+import unittest
+from joke.fortuneCookieService import FortuneCookieService
+
+FCS = FortuneCookieService()
+
+
+class TestFortuneCookieService(unittest.TestCase):
+    def test_fortuneCookieServiceRequest_type(self):
+        self.assertIsInstance(FCS.fortuneCookieServiceRequest(),str)
+
+    def test_fortuneCookieServiceRequest_content(self):
+        correct_string = "He’s not the Messiah , he’s a very naughty boy."
+        self.assertEqual(correct_string,FCS.fortuneCookieServiceRequest())
