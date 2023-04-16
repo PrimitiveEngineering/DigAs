788cae823fa52aa8330adeb28504ffebb632314c
Tomasz Kania <101673515+TomaszKania2000@users.noreply.github.com>
2023-04-16 13:28:25 +0200

    Changes to calm Kevs mind

--- test/test_musicApi.py
+++ test/test_musicApi.py
@@ -10,17 +10,19 @@ from cooking.musicApi import MusicApi
 
 MA = MusicApi()
 
+
 class TestMusicApi(TestCase):
 
     def test_play_requested_song(self):
-        self.assertEqual(None,MA.play_requested_song("Days of Blues"))
+        self.assertEqual(None, MA.play_requested_song("Days of Blues"))
+
     def test_search_song_uri(self):
         result_string = "spotify:track:6fLnMSHygIfRzdfPlplrCc"
-        self.assertEqual(result_string,MA.search_song_uri("Days of Blues"))
+        self.assertEqual(result_string, MA.search_song_uri("Days of Blues"))
 
     def test_build_requested_song_string(self):
         result_string = "TITEL artist:ARTIST"
-        self.assertEqual(result_string,MA.build_requested_song_string("TITEL","ARTIST"))
+        self.assertEqual(result_string, MA.build_requested_song_string("TITEL", "ARTIST"))
 
     def test_start_song(self):
-        self.assertEqual(None,MA.start_song("spotify:track:6fLnMSHygIfRzdfPlplrCc"))
+        self.assertEqual(None, MA.start_song("spotify:track:6fLnMSHygIfRzdfPlplrCc"))
