4a1c5a2821deec8b52fec760d7fc4c858d46ba5e
Max <maxsydneyduerr@gmail.com>
2023-04-18 19:22:39 +0200

    -timemngrCon start function done -job create to do

--- timemgr/calenderAPI.py
+++ timemgr/calenderAPI.py
@@ -1,10 +1,18 @@
 from gcsa.event import Event
 from gcsa.google_calendar import GoogleCalendar
-from gcsa.recurrence import Recurrence, DAILY, SU, SA
 
 from beautiful_date import *
 
-class calenderAPI:
+
+class CalenderAPI:
+
+    __instance = None
+
+    # singleton pattern
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
 
     def __init__(self):
         self.calendar = GoogleCalendar('primitiveengineeringdhbw@gmail.com',
@@ -12,18 +20,34 @@ class calenderAPI:
 
     def print_events(self):
         for event in self.calendar:
+            print(event.event_id)
             print(event)
 
-    def print_events(self, start, end):
-        for event in self.calendar.get_events(start, end, order_by='startTime'):
-            print(event)
+    # def print_events(self, start, end):
+    #     for event in self.calendar.get_events(start, end, order_by='startTime'):
+    #         print(event)
 
     def create_event(self, name, start, end):
         event = Event(name, start, end)
         self.calendar.add_event(event)
 
+    def get_event_id(self, event_id):
+        return self.calendar.get_event(event_id)
+
+    def get_events_24h(self):
+        return list(self.calendar.get_events(D.now(), D.now() + 1 * days, order_by='startTime', single_events=True))
+
+    def get_events_24h_list(self):
+        times = []
+        for event in self.calendar.get_events(D.now(), D.now() + 1 * days, order_by='startTime', single_events=True):
+            times.append((event.start, event.end))
+        return times
     def get_events(self, start, end):
-        return self.calendar.get_events(start, end, order_by='startTime')
+        return list(self.calendar.get_events(start, end, order_by='startTime', single_events=True))
 
     def get_events_now(self):
-        return self.calendar.get_events(D.now(), order_by='startTime')
+        return list(self.calendar.get_events(D.now(), D.now() + 1 * minutes, order_by='startTime', single_events=True))
+
+    def is_event_now(self):
+        return len(list(
+            self.calendar.get_events(D.now(), D.now() + 1 * minutes, order_by='startTime', single_events=True))) > 0
